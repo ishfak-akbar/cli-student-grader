98cab671f9e7c279d3cf8830e7e0dfddc3807681
ishfak-akbar <ishfakakbar24@gmail.com>
2026-04-10 07:09:21 +0600

    feat: added menu loop and switch routing

--- cli_student_grader/bin/cli_student_grader.dart
+++ cli_student_grader/bin/cli_student_grader.dart
@@ -1,5 +1,49 @@
-import 'package:cli_student_grader/cli_student_grader.dart' as cli_student_grader;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${cli_student_grader.calculate()}!');
+void main() {
+  const appTitle = "Student Grader v1.0";
+  List<Map<String, dynamic>> students = [];
+  final Set<String> subjects = {"English", "Bangla", "Math", "Science"};
+  var option="";
+  do {
+    String menu =
+        """
+===== $appTitle =====
+
+1. Add Student
+2. Record Score
+3. Add Bonus Points
+4. Add Comment
+5. View All Students
+6. View Report Card
+7. Class Summary
+8. Exit
+
+Choose an option:
+""";
+    print(menu);
+
+    option = stdin.readLineSync() ?? "";
+    switch (option) {
+      case "1":
+        break;
+      case "2":
+        break;
+      case "3":
+        break;
+      case "4":
+        break;
+      case "5":
+        break;
+      case "6":
+        break;
+      case "7":
+        break;
+      case "8":
+        print("Exiting...");
+        break;
+      default:
+        print("Invalid option. Try again.");
+    }
+  } while (option != "8");
 }
